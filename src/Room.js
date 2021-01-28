06ff69eaab23e39dab7c02ddf65a188bb1b069d5
MAtifJaved <atif_javed1992@yahoo.com>
2021-01-28 11:02:01 +0500

    room.css file added

--- src/Room.js
+++ src/Room.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './Room.css';
 //console.log("State = ",state);
 //const state=useState(true);
 
@@ -16,23 +17,41 @@ function ageUpdated(){
      console.log("age is being updated")
      setAge(++age)
 }
+//methods to include dynamic values to strings
+
+//(1)
+//this line was used in ES6 for concatenation of string
+//let hello= 'hello' + islit + 'world';      
+
+//(2)
+// now the following line is used
+// let hello= 'hello ${islit} world' ;
 
 
 return (
-    <div>
+    <div className={`room ${islit? "lighted": "darken"}`}>
        this room is {islit? "lighted": "Darken"};
        <br/>
-       
+
        age: {age}
+
        <br/>
+
        <button onClick={()=>setIslit(!islit)}>Toggle light</button>
+
        <br/>
+
         <button onClick={() => {
                 console.log("age is being updated");
                 setAge(++age);
                 }} > 
-                Toggle age
+                <strong>Increaseage</strong> 
         </button>
+
+        <br/>
+
+        <button onClick={()=>{setAge(--age)}}>Decrease Age</button>
+
     </div>
   );
 
